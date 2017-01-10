f2b4328f64b77ef218297cdcdd324174de133e04
Rubiksron <rubiksron@gmail.com>
2017-01-10 11:47:38 -0800

    I transfered all lab-13 code to the lab-14 folder to finish up the assignment there. I also renamed dogs.js to dog.js, which was a typo, thanks for catching that, Scott.

---
+++ lab-14-two-resources/routes/chew-toy-routes.js
@@ -0,0 +1,42 @@
+'use strict'
+
+let Router = require('express').Router
+let jsonParser = require('body-parser').json()
+let Dog = require('../models/dog.js')
+let ChewToy = require('../models/chew-toy.js')
+let router = module.exports = new Router()
+
+router.post('/dog/:id/chewtoy', jsonParser, (req, res, next) => {
+  console.log('in post')
+  let newChewToy;
+  new ChewToy(req.body).save()
+  .then(chewToy => {
+    newChewToy = chewToy
+    return Dog.findById(req.params.id)
+  })
+  .then(dog => {
+    dog.chewToy.push(newChewToy.id)
+    return dog.save()
+  })
+  .then(newChewToy => res.send(newChewToy))
+  .catch(next)
+})
+
+router.get('/chewtoy/:id', (req, res, next) => {
+  ChewToy.findById(req.params.id)
+  .then(chewToy => res.json(chewToy))
+  .catch(next)
+})
+
+router.put('/chewtoy/:id', (req, res, next) => {
+  ChewToy.findByIdAndUpdate(req.params.id)
+  .then(chewToy => res.json(chewToy))
+  .catch(next)
+})
+
+router.delete('/chewtoy/:id', (req, res, next) => {
+  console.log('deleted file ')
+  ChewToy.findByIdAndRemove(req.params.id)
+  .then(chewToy => res.json(chewToy))
+  .catch(next)
+})
